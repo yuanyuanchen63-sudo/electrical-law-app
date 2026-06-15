ed62c32e0ad66e35a98a3cf58ceaff0347a57196
yuanyuanchen63-sudo <yuanyuanchen63@gmail.com>
2026-06-15 15:53:58 +0800

    Update manifest.ts

--- Desktop/electrical-law-app/app/manifest.ts
+++ Desktop/electrical-law-app/app/manifest.ts
@@ -11,11 +11,14 @@ export default function manifest() {
     theme_color: "#0B1220",
 
     icons: [
-      {
-        src: "/icon-192.png",
-        sizes: "192x192",
-        type: "image/png"
-      }
-    ]
-  };
-}
+  {
+    src: "/icon-192.png",
+    sizes: "192x192",
+    type: "image/png"
+  },
+  {
+    src: "/icon-512.png",
+    sizes: "512x512",
+    type: "image/png"
+  }
+]
